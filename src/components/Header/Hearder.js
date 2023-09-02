bea1e557baa9443ae36919162dad72c23c2edfc1
naveenthangaraj02 <130845490+naveenthangaraj02@users.noreply.github.com>
2023-09-02 20:56:48 +0530

    boa logo added

--- src/components/Header/Hearder.js
+++ src/components/Header/Hearder.js
@@ -1,4 +1,5 @@
 import logo from "../../assets/investment-calculator-logo.png";
+import boa from "../../assets/boa.jpg";
 
 const Header = () => {
   return (
@@ -7,6 +8,9 @@ const Header = () => {
         <img src={logo} alt="logo" />
         <h1>The Best Investment Calculator</h1>
       </header>
+      <div>
+        <img src={boa} alt="boa-logo" />
+      </div>
     </div>
   );
 };
