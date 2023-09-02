f63d82de5913ac28bfe3bf47e83851d917fb49a4
naveenthangaraj02 <130845490+naveenthangaraj02@users.noreply.github.com>
2023-09-02 20:43:57 +0530

    Title has Changed

--- src/components/Header/Hearder.js
+++ src/components/Header/Hearder.js
@@ -1,13 +1,14 @@
-import logo from '../../assets/investment-calculator-logo.png'
+import logo from "../../assets/investment-calculator-logo.png";
 
 const Header = () => {
-
-    return <div>
-        <header className="header">
+  return (
+    <div>
+      <header className="header">
         <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
+        <h1>The Best Investment Calculator</h1>
       </header>
     </div>
-}
+  );
+};
 
 export default Header;
